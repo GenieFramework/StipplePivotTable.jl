36bcaffdc3c2677d5455c6385ee3a28364010774
Adrian Salceanu <adrian.salceanu@gmail.com>
2025-01-07 20:32:17 +0100

    Julia backend API

--- demo/pivot.jl
+++ demo/pivot.jl
@@ -1,39 +1,66 @@
 module App
 using GenieFramework
 @genietools
-using StipplePivotTable, DataFrames, JSON3
+using StipplePivotTable; const spt = StipplePivotTable
+using DataFrames, JSON3
 
 # Load and parse source data
 json_file = "data/Employee_Sample_Data.json"
 json_string = read(json_file, String)
 data = JSON3.read(json_string)
 
-# create a DataFrame
-dataframe = DataFrame(data)
+# StipplePivotTable.Cell("EEID", "label", "asc", "EEID")
+# StipplePivotTable.Cell("Full Name", "label", "asc", "Full Name")
+# StipplePivotTable.Cell("Job Title", "label", "asc", "Job Title")
+# StipplePivotTable.Cell("Department", "label", "asc", "Department")
+# StipplePivotTable.Cell("Business Unit", "label", "asc", "Business Unit")
+# StipplePivotTable.Cell("Gender", "label", "asc", "Gender")
+# StipplePivotTable.Cell("Ethnicity", "label", "asc", "Ethnicity")
+# StipplePivotTable.Cell("Age", "label", "asc", "Age")
+# StipplePivotTable.Cell("Hire Date", "label", "asc", "Hire Date")
+# StipplePivotTable.Cell("Annual Salary", "label", "asc", "Annual Salary")
+# StipplePivotTable.Cell("Bonus", "label", "asc", "Bonus")
+# StipplePivotTable.Cell("Country", "label", "asc", "Country")
+# StipplePivotTable.Cell("City", "label", "asc", "City")
+# StipplePivotTable.Cell("Exit Date", "label", "asc", "Exit Date")
 
+# create a pivot table
+pt = PivotTable(
+    DataFrame(data),
+    PivotTableOptions(
+        # rows = [
+        #     # Cell("Business Unit"),
+        #     # Cell("Department", order = :desc)
+
+        #     Cell("Business Unit", "label", "asc", "Business Unit"),
+        #     Cell("Department", "label", "asc", "Department")
+        # ],
+        # columns = [
+        #     # Cell("Gender", order = :desc),
+        #     # Cell("Ethnicity")
+
+        #     Cell("Gender", "label", "asc", "Gender"),
+        #     Cell("Ethnicity", "label", "asc", "Ethnicity")
+        # ],
+        rows = spt.rows(["Business Unit", "Department"]),
+        columns = spt.columns(["Gender", "Ethnicity"]),
+        values = [
+            Value("Annual Salary", aggregation = "sum"),
+            Value("Annual Salary", aggregation = "custom", formula = "{Annual Salary} * 0.02")
+        ],
+        filters = [
+            Filter("Annual Salary", type = "condition", condition = "greaterThan", value = 220000),
+            Filter("Country", type = "values", condition = "contains", selected_values = ["China", "Brazil"])
+        ]
+    )
+)
 
 @app begin
-    # Configure the pivot table properties
-    @in rows = [
-        Dict(:field => "Business Unit", :sortBy => "label", :sortOrder => "asc"),
-        Dict(:field => "Department", :sortBy => "label", :sortOrder => "desc")
-    ]
-    @in columns = [
-        Dict(:field => "Gender", :sortBy => "label", :sortOrder => "desc"),
-        Dict(:field => "Ethnicity", :sortBy => "label", :sortOrder => "asc")
-    ]
-    @in values = [
-        Dict(:field => "Annual Salary", :aggregation => "sum" ), 
-        Dict(:field => "Annual Salary", :aggregation => "custom", :formula => " {Annual Salary} * 0.01 " )
-    ]
-
-    @in filters = [
-        Dict(:column => "Annual Salary", :filterType => "condition", :condition => "greaterThan", :value => 220000), 
-        Dict(:column => "Country", :filterType => "values", :condition => "contains", :selectedValues => ["China", "Brazil"] )
-    ]
-
-    # expose data binding
-    @out dataframe = dataframe
+    @out rows = pt.opts.rows
+    @out columns = pt.opts.columns
+    @out values = pt.opts.values
+    @out filters = pt.opts.filters
+    @out data = pt.data
 end
 
 # serve the app
--- src/StipplePivotTable.jl
+++ src/StipplePivotTable.jl
@@ -1,9 +1,9 @@
 module StipplePivotTable
 
-using Stipple, StippleUI.API
+using Stipple, StippleUI.API, Stipple.ReactiveTools
 import Tables as TablesInterface
 
-export pivottable
+export pivottable, Cell, Value, Filter, PivotTableOptions, PivotTable
 
 const assets_config = Genie.Assets.AssetsConfig(package="StipplePivotTable.jl")
 
@@ -18,30 +18,65 @@ const CONDITIONS = [:greaterThan, :lessThan, :greaterThanOrEqualTo, :lessThanOrE
 @kwdef mutable struct Cell
     field::Union{String, Symbol}
     sort_by::Union{String, Symbol} = "label"
-    sort_order::Union{String, Symbol} = "asc"
-    label::Union{String, Symbol, Nothing} = nothing
+    order::Union{String, Symbol} = "asc"
+    label::Union{String, Symbol} = field
 end
+Cell(field::Union{String, Symbol}; kwargs...) = Cell(; field, kwargs...)
+
+function Stipple.render(cell::Cell)
+    return Dict(:field => cell.field, :sortBy => cell.sort_by, :sortOrder => cell.order, :label => cell.label)
+end
+Stipple.render(cells::Vector{Cell}) = Stipple.render([Stipple.render(cell) for cell in cells])
 
 @kwdef mutable struct Value
     field::Union{String, Symbol}
     aggregation::Union{String, Symbol}
     formula::Union{String, Symbol, Nothing} = nothing
 end
+Value(field::Union{String, Symbol}; kwargs...) = Value(; field, kwargs...)
+
+function Stipple.render(value::Value)
+    d = Dict(:field => value.field, :aggregation => value.aggregation)
+    if value.formula !== nothing
+        d[:formula] = " $(value.formula) " # this is needed to allow {field} in the formula to be rendered correctly
+    end
+
+    return d
+end
+Stipple.render(values::Vector{Value}) = Stipple.render([Stipple.render(value) for value in values])
 
 @kwdef mutable struct Filter
-    column::Union{String, Symbol, Nothing} = nothing
-    row::Union{String, Symbol, Nothing} = nothing
-    type::Union{String, Symbol}
+    field::Union{String, Symbol}
     condition::Union{String, Symbol}
-    value::Union{String, Symbol, Nothing} = nothing
-    selected_values::Union{Vector{String}, Nothing} = nothing
+    type::Union{String, Symbol, Nothing} = nothing
+    value::Union{Any, Nothing} = nothing
+    selected_values::Union{Any, Vector, Nothing} = nothing
 end
+Filter(field::Union{String, Symbol}; kwargs...) = Filter(; field, kwargs...)
+
+function Stipple.render(filter::Filter)
+    if filter.type === nothing && filter.selected_values !== nothing && filter.value === nothing
+        filter.type = :values
+    elseif filter.type === nothing && filter.selected_values === nothing && filter.value !== nothing
+        filter.type = :condition
+    end
+
+    return Dict(:field => filter.field, :filterType => filter.type, :condition => filter.condition, :value => filter.value, :selectedValues => filter.selected_values)
+end
+Stipple.render(filters::Vector{Filter}) = Stipple.render([Stipple.render(filter) for filter in filters])
 
 @kwdef mutable struct PivotTableOptions
-    rows::Vector{Cell}
-    columns::Vector{Cell}
-    values::Vector{Value}
-    filters::Vector{Filter}
+    rows::Vector{Cell} = []
+    columns::Vector{Cell} = []
+    values::Vector{Value} = []
+    filters::Vector{Filter} = []
+end
+
+function Stipple.render(opts::PivotTableOptions)
+    return Dict(:rows => [Stipple.render(cell) for cell in opts.rows],
+                :columns => [Stipple.render(cell) for cell in opts.columns],
+                :values => [Stipple.render(value) for value in opts.values],
+                :filters => [Stipple.render(filter) for filter in opts.filters])
 end
 
 @kwdef mutable struct PivotTable{T}
@@ -49,9 +84,67 @@ end
     opts::PivotTableOptions
 end
 
+function Stipple.render(pt::PivotTable)
+    isempty(pt.opts.rows) && (pt.opts.rows = rows(pt))
+    isempty(pt.opts.columns) && (pt.opts.columns = columns(pt))
 
-function pivottable(;kwargs...)
-    st_pivottable(;kw([kwargs...])...)
+    return Dict(:data => data(pt), :options => Stipple.render(pt.opts))
+end
+
+function columns(pivot::PivotTable) :: Vector{Cell}
+    pivot.opts.columns !== nothing ?
+        pivot.opts.columns :
+            [Cell(field = string(name), label = string(name)) for name in TablesInterface.columnnames(pivot.data)]
+end
+columns(data) = [Cell(field = string(name), label = string(name)) for name in TablesInterface.columnnames(data)]
+columns(names::Vector{String}) = [Cell(field = name, label = name) for name in names]
+
+function rows(pivot::PivotTable) :: Vector{Cell}
+    pivot.opts.rows !== nothing ?
+        pivot.opts.rows :
+            [Cell(field = string(name), label = string(name)) for name in TablesInterface.columnnames(pivot.data)]
+end
+rows(data) = [Cell(field = string(name), label = string(name)) for name in TablesInterface.columnnames(data)]
+rows(names::Vector{String}) = [Cell(field = name, label = name) for name in names]
+
+function values(pivot::PivotTable) :: Vector{Value}
+    pivot.opts.values
+end
+
+function filters(pivot::PivotTable) :: Vector{Filter}
+    pivot.opts.filters
+end
+
+function data(pivot::PivotTable)
+    TablesInterface.table(pivot.data)
+end
+
+function pivottable(pt::PivotTable; columns::Union{Vector{Cell}, Nothing} = nothing,
+                                    rows::Union{Vector{Cell}, Nothing} = nothing,
+                                    values::Union{Vector{Value}, Nothing} = nothing,
+                                    filters::Union{Vector{Filter}, Nothing} = nothing)
+    columns = columns !== nothing ? columns : columns(pt)
+    rows = rows !== nothing ? rows : rows(pt)
+    values = values !== nothing ? values : values(pt)
+    filters = filters !== nothing ? filters : filters(pt)
+
+    return st__pivottable(data = pt.data; columns, rows, values, filters)
+end
+
+# doesn't work yet due to Stipple bug
+# macro pivotvars(varname)
+#     # Generate the required expressions
+#     quote
+#         @out $(esc(varname))_rows = $(esc(varname)).opts.rows
+#         @out $(esc(varname))_columns = $(esc(varname)).opts.columns
+#         @out $(esc(varname))_values = $(esc(varname)).opts.values
+#         @out $(esc(varname))_filters = $(esc(varname)).opts.filters
+#         @out $(esc(varname))_data = $(esc(varname)).data
+#     end
+# end
+
+function pivottable(; kwargs...)
+    st_pivottable(; kw([kwargs...])...)
 end
 
 function gb_component_routes()
