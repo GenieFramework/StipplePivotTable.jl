61e16d030ccdaef322f9cc8ce0fd70169d57c23a
Adrian Salceanu <essenciary@users.noreply.github.com>
2025-01-28 17:15:18 +0100

    Fix formatter type

--- src/StipplePivotTable.jl
+++ src/StipplePivotTable.jl
@@ -89,7 +89,7 @@ Base.Dict(formatter::T) where T <: ValueFormatter = Dict(
 )
 
 function Stipple.render(formatter::T) where T <: ValueFormatter
-    type = replace(string(typeof(formatter)), "Formatter" => "") |> lowercase
+    type = replace(string(typeof(formatter) |> nameof), "Formatter" => "") |> lowercase
     Dict(:type => type, :config => Dict(
         [Symbol(snake_to_camel(k)) => v for (k, v) in Dict(formatter)]
     ))
